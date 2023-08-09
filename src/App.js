2780f5b337b2cec049d1f0108d1bdead1f3ec0c3
sbodrero <bodrero.sebastien@gmail.com>
2023-08-09 14:21:02 +0200

    Applied props

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ function App() {
     {name: 'Roger', city: 'Tour'},
     {name: 'Cécile', city: 'Angers'},
   ]
+
+  const getUsersCount = users => users.length;
+  const pageTitle = "Les des utilisateurs";
+  const year = 2023;
+  const style = { backgroundColor: 'white' };
+
   return (
     <div className='App'>
       <h1>Hello React.</h1>
-      <MainContent users={users} />
+      <MainContent users={users} getUsersCount={getUsersCount} title={pageTitle} year={year} style={style} />
     </div>
   );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,12 +2,12 @@ import React from 'react';
 import UserCount from './UserCount';
 import UsersList from './UsersList';
 
-export default function MainContent({ users }) {
+export default function MainContent({ users, getUsersCount, title, year, style }) {
   return (
-    <div style={{ backgroundColor: 'white'}}>
+    <div style={style}>
       <h1>Accueil</h1>
-      <h2>Liste des utilisateurs</h2>
-      <UserCount users={users} />
+      <h2>{title} of {year}</h2>
+      <UserCount users={users} getUsersCount={getUsersCount} />
       <UsersList users={users} />
     </div>
   )
