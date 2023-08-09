b43321615ba6db77f180613dc630eb4c388a7d19
sbodrero <bodrero.sebastien@gmail.com>
2023-08-09 15:13:45 +0200

    Added li key

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function UsersList({ users }) {
+  console.log(users);
   return (
     <div style={{ backgroundColor: 'green', padding: '50px' }}>
       {users.length > 0 && (
           <ul>
-            {users.map(user => <li>Prénom: {user?.name} - Ville: {user?.city}</li>)}
+            {users.map(user => <li key={user.name}>Prénom: {user?.name} - Ville: {user?.city}</li>)}
           </ul>
         )
       }
